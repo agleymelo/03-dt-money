1e86277a4c8f15498a5b068f786cf972a46aea3c
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-20 19:18:49 -0300

    Estrutura visiual - Tabela de transacoes

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,40 @@
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
-import { Container } from "./styles"
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles"
 
 export function Transactions() {
   return (
-    <Container>
+    <div>
       <Header />
       <Summary />
-    </Container>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="40%">Hambúrguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 1.200,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
   )
 }
